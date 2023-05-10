872518e1d89102c49169e4e4a57eb0d892e5f63d
Diego Saggioro <sad2wrn@bosch.com>
2023-05-09 23:31:06 -0300

    feat: pagina inicial

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import backgroundImage from "./assets/background.png";
+import logo from "./assets/logo.png";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "PlayfairDisplay-Black": require("./assets/fonts/PlayfairDisplay-Black.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={backgroundImage} style={styles.container}>
+        <View style={styles.container}>
+          <Image source={logo} style={styles.logo} />
+          <Text style={styles.title}>Vacina PET</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text>Entrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>Crie sua conta</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +40,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  logo: {
+    width: "8rem",
+    height: "8rem",
+  },
+  title: {
+    fontSize: "3rem",
+    fontWeight: 900,
+    fontFamily: "PlayfairDisplay-Black",
+    marginBottom: 60,
+  },
+  button: {
+    width: "45%",
+    height: "2.5rem",
+    backgroundColor: "#fff",
+    marginBottom: 12,
+    borderRadius: 8,
+    border: "1px solid #8C6F4A",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
